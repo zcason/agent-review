c3b6e1ad994ef50dff3796dcad0add463e7ef1cc
zcason <zuri.cason@gmail.com>
2021-07-20 15:05:09 -0500

    added post services for adding agents

--- server/app.js
+++ server/app.js
@@ -8,4 +8,38 @@ app.get('/agents', async (req, res, next) => {
   return res.json(agents);
 });
 
+app.post('/agents', async (req, res, next) => {
+  const { firstName, lastName, photoUrl, agentLicence, address, practiceAreas, aboutMe } = req.body.newAgent;
+
+  // checks to see if rquired fileds are missing 
+  for (const field of ['firstName', 'lastName', 'agentLicence', 'address']){
+    if (!req.body[field])
+      return res.status(400).json({error: `Missing '${field}' in request body`})
+  }
+
+  try {
+    await Agent.create({
+      firstName,
+      lastName,
+      photoUrl,
+      agentLicence,
+      address,
+      practiceAreas,
+      aboutMe 
+  });
+  return res.status(201);
+
+} catch (error) {
+    next(error)
+}
+
+  
+});
+
+
+// app.get('agents/:id', async (req, res, next) => {
+
+// })
+
+
 module.exports = app;
