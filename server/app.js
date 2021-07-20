2f8b92ea02ed3ab956e719bdaa48286fd5839d23
zcason <zuri.cason@gmail.com>
2021-07-20 16:01:01 -0500

    post service for adding new agents updated

--- server/app.js
+++ server/app.js
@@ -3,14 +3,15 @@ const { restart } = require('nodemon');
 const { Agent } = require('./model');
 
 const app = express();
+const jsonBodyParser = express.json();
 
 app.get('/agents', async (req, res, next) => {
   const agents = await Agent.findAll();
   return res.json(agents);
 });
 
-app.post('/agents', async (req, res, next) => {
-  const { firstName, lastName, photoUrl, agentLicence, address, practiceAreas, aboutMe } = req.body.newAgent;
+app.post('/agents', jsonBodyParser, async (req, res, next) => {
+  const { firstName, lastName, photoUrl, agentLicence, address, practiceAreas, aboutMe } = req.body;
 
   // checks to see if rquired fileds are missing 
   for (const field of ['firstName', 'lastName', 'agentLicence', 'address']){
@@ -18,8 +19,7 @@ app.post('/agents', async (req, res, next) => {
       return res.status(400).json({error: `Missing '${field}' in request body`})
   }
 
-  try {
-    await Agent.create({
+    const newAgent = await Agent.create({
       firstName,
       lastName,
       photoUrl,
@@ -28,46 +28,41 @@ app.post('/agents', async (req, res, next) => {
       practiceAreas,
       aboutMe 
   });
-  return res.status(201);
 
-} catch (error) {
-    next(error)
-}
-
-  
+  return res
+    .status(201)
+    .json(newAgent);  
 });
 
-
+restart()
 // app.get('agents/:id', async (req, res, next) => {
 
 // })
 
-app.post('agents/:id', async (req, res, next) => {
-  const { review } = req.body;
-  const newReview = {
-    review,
-    agent: req.params.id
-  };
+// app.post('agents/:id', jsonBodyParser, async (req, res, next) => {
+//   const { review } = req.body;
+//   const newReview = {
+//     review,
+//     agent: req.params.id
+//   };
 
-  if (!req.body.review)
-    return res.status(400).json({
-      error: "Missing 'review' in request body"
-  })
+//   if (!req.body.review)
+//     return res.status(400).json({
+//       error: "Missing 'review' in request body"
+//   })
 
-  try {
-    // await Review.create({
-    //   review,
-    //   agent: req.params.id
-    // });
-    console.log(newReview)
-  return res.status(201);
+//   try {
+//     // await Review.create({
+//     //   review,
+//     //   agent: req.params.id
+//     // });
+//     console.log(newReview)
+//   return res.status(201);
 
-} catch (error) {
-    next(error)
-}
-
-
-})
+// } catch (error) {
+//     next(error)
+// }
+// })
 
 
 module.exports = app;
